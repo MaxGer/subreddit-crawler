2d534c98f2c116dbe43d35599cc7d13beebf17ba
LucasG04 <lucasgolenia@gmail.com>
2021-04-29 11:36:51 +0200

    adds logic for filtering post headlines with cmd output

---
+++ backend/main.py
@@ -0,0 +1,143 @@
+import requests
+import re
+import time
+from datetime import datetime
+from string import punctuation
+from operator import itemgetter
+from collections import Counter
+from Data.big_mock_data import big_mock_posts
+
+#############
+#
+# Algorithm tries to filter the trends of r/wallstreetbets
+# The following steps are performed:
+# - read 100 hot headlines & texts
+# - remove words that appear in a standard word list
+# - run against a blacklist and delete all found words (YOLO, FOMO, DD, etc.)
+# - delete single letters
+# - remove duplicate words
+#
+#############
+
+#
+def get_reddit_posts(sort = 'new'):
+    allposts = []
+    after = ''
+
+    i = 1
+    while i < 10:
+        while True:
+            payload = {'limit': '50', 'after': after}
+            response = requests.get(f'https://www.reddit.com/r/wallstreetbets/{sort}.json', params=payload)
+            posts = response.json()
+            if 'data' in posts and posts['data'] is not None:
+                if ('children' in posts['data'] and posts['data']['children'] is not None):
+                    after = posts['data']['after']
+                    allposts.extend(posts['data']['children'])
+                    newpostcount = len(posts['data']['children'])
+                    print(f'#{i}: Received {newpostcount} new posts.')
+                break
+            else:
+                if 'error' in posts and posts['error'] == 429:
+                    print(f'#{i}: Request blocked. Trying again...')
+                else:
+                    print(posts)
+                time.sleep(5)
+        
+        if (post_is_stale(allposts[-1])):
+            print(f'Posts are out of date after request #{i}.')
+            break
+        i += 1
+    
+    return allposts
+
+## cheks if the given post is older than 1 day
+def post_is_stale(post):
+    createdAt = datetime.utcfromtimestamp(post['data']['created'])
+    time_between = datetime.now() - createdAt
+    return time_between.days > 1
+
+# Merge the posts to one array
+def merge_posts(posts):
+    full_text = ''
+    for post in posts:
+        if ('title' in post['data'] and post['data']['title'] is not None):
+            title_without_duplicates = uniquify(post['data']['title'])
+            full_text += title_without_duplicates
+        # if ('selftext' in post['data'] post['data']['selftext'] is not None):
+        #     text_without_duplicates = uniquify(post['data']['selftext'])
+        #     full_text += text_without_duplicates
+
+    return full_text
+
+def uniquify(string):
+    output = []
+    seen = set()
+    for word in string.split():
+        if word not in seen:
+            output.append(word)
+            seen.add(word)
+    return ' '.join(output)
+
+# filter the given text
+def filter_text(full_text):
+    with open('./Data/english_words.txt') as f:
+        common_words = f.readlines()
+    common_words = [x.strip() for x in common_words]
+
+    with open('./Data/blacklist_words.txt') as f:
+        blacklist = f.readlines()
+    blacklist = [x.strip() for x in blacklist]
+
+    filtered_text = full_text
+    common_words.extend(blacklist)
+    for word in common_words:
+        filtered_text = filtered_text.replace(word, '')
+    
+    filtered_text = clean_text(filtered_text)
+    return filtered_text
+
+# removes single char strings form string
+def clean_text(text):
+    cleaned_text = text
+    cleaned_text = ' '.join( [w for w in cleaned_text.split() if len(w)>1] )
+    cleaned_text = ' '.join(cleaned_text.split())
+    return cleaned_text
+
+# count words
+def count_words(text):
+    words = {}
+    words_gen = (word.strip(punctuation).lower() for line in text for word in line.split())
+
+    for word in words_gen:
+        words[word] = words.get(word, 0) + 1
+
+    top_words = Counter(words_gen).most_common(N)
+
+    mentions = []
+    for word, frequency in top_words:
+        mentions.append(StockMention(word, frequency))
+    
+    return mentions
+
+## removes mentions with a frequency below 5 and removes strings with numbers
+def filter_irrelevat_mentions(mentions):
+    relevant_mentions = []
+    for mention in mentions:
+        if (mention[1] > 4 and not hasNumbers(mention[0])):
+            relevant_mentions.append(mention)
+
+    return relevant_mentions
+
+### checks if the given string contains numbers
+def hasNumbers(inputString):
+    return bool(re.search(r'\d', inputString))
+
+if __name__ == '__main__':
+    posts = get_reddit_posts()
+    full_text = merge_posts(posts)
+    filtered_text = filter_text(full_text)
+    mentions = Counter(filtered_text.split()).most_common()
+    analysis_result = filter_irrelevat_mentions(mentions)
+
+    print(analysis_result)
